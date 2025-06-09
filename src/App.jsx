ddd54a43161221783b36522810d11e3b2ebba065
Ameen224 <ameen8714228@gmail.com>
2025-06-09 11:40:15 +0530

    convert app and index into one

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 // src/App.jsx
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import './style/App.css';
 
 const App = () => {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+      <DndProvider backend={HTML5Backend}>
+        <AppRoutes />
+      </DndProvider>
+    </BrowserRouter>
+  );
 };
 
-export default App;
+// Mount the app to the root div
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
